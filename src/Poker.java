12debbdb136eb911a20a6ee3f924a48cfa9c513c
BT_2S14_08 <hlebp@nycstudents.net>
2025-01-28 10:18:08 -0500

    Started poker class

--- src/Poker.java
+++ src/Poker.java
@@ -1,3 +1,25 @@
 public class Poker {
     private String[] bid;
+    private String[] cards;
+
+ public Poker (String[] bid, String[] cards){
+     this.bid = bid;
+     this.cards = cards;
+ }
+
+ public String analyzeHand(){
+    String card1 = cards[0];
+     String card2 = cards[1];
+     String card3 = cards[2];
+     String card4 = cards[3];
+     String card5 = cards[4];
+     int group1 = 0;
+     int group2 = 0;
+
+
+
+
+
+ }
+
 }
