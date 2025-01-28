1f45ca60e4f0d34965ee776dc8f6128cdd828389
BT_2S14_07 <eli.jl.dew@gmail.com>
2025-01-28 10:26:24 -0500

    card separation

--- src/Poker.java
+++ src/Poker.java
@@ -5,16 +5,29 @@ public class Poker {
  public Poker (String[] bid, String[] cards){
      this.bid = bid;
      this.cards = cards;
+     int group1 = 0;
+     int group2 = 0;
  }
 
  public String analyzeHand(){
-    String card1 = cards[0];
-     String card2 = cards[1];
-     String card3 = cards[2];
-     String card4 = cards[3];
-     String card5 = cards[4];
-     int group1 = 0;
-     int group2 = 0;
+     String card1 = "";
+     String card2 = "";
+     String card3 = "";
+     String card4 = "";
+     String card5 = "";
+
+    for (String pile : cards){
+        card1 = pile.substring(0,(pile.indexOf(",")));
+        pile = pile.substring((pile.indexOf(",")));
+        card2 = pile.substring(0,(pile.indexOf(",")));
+        pile = pile.substring((pile.indexOf(",")));
+        card3 = pile.substring(0,(pile.indexOf(",")));
+        pile = pile.substring((pile.indexOf(",")));
+        card4 = pile.substring(0,(pile.indexOf(",")));
+        pile = pile.substring((pile.indexOf(",")));
+        card5 = pile;
+
+    }
 
 
 
