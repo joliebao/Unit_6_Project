9fce592c5e3f2880a023f7bca02fc201ce8829e2
BT_2S14_07 <eli.jl.dew@gmail.com>
2025-01-28 10:17:26 -0500

    Deck separation
    Input file's elements are separated into proper card decks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] hand = getFileData("src/InputFile");
+        System.out.println(Arrays.toString(hand));
+//        Poker poker = new Poker(hand);
+
+    }
+
+    public static String[] getFileData(String fileName) {
+        String[] hand = new String[5];      // change based on number of lines on input file
+        File f = new File("src/input_file");
+
+        String line = "";
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String currentLine = s.nextLine();
+                line += currentLine.substring(0, currentLine.indexOf("|")) + "\n";
+            }
+
+            String[] array = line.split("\n");
+
+            for (int i = 0; i < array.length; i++) {
+                // split by space, now we have a list of String numbers
+                hand[i] = Arrays.toString((array[i]).split(" "));
+            }
+            return hand;
+        } catch (FileNotFoundException fe) {
+            System.out.println("File was not found");
+            System.exit(1);
+        }
+        return hand;
     }
 }
