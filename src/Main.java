50e92074131873b269dc6f5da610475c8b668837
joliebao <baojolie@gmail.com>
2025-01-29 21:52:08 -0500

    I rearranged some of the code in part 1, to make it run more efficiently (poker class) and also made a bid array (main class)
    I am working on testing and debugging Part 1

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] hand = getFileData("src/InputFile");
+        String[] hand = getCards("src/InputFile");
+        int[] bet = getBet("src/InputFile");
+
+        System.out.println(Arrays.toString(bet));
         System.out.println(Arrays.toString(hand));
-        Poker poker = new Poker(hand);
+        Poker poker = new Poker(hand, bet);
         System.out.println(poker.analyzeHand());
     }
 
-    public static String[] getFileData(String fileName) {
-        String[] hand = new String[5];      // change based on number of lines on input file
+    // METHOD: all card hands are made into a STRING array
+    public static String[] getCards(String fileName) {
+        String[] hand = new String[79];      // change based on number of lines on input file
         File f = new File("src/input_file");
 
-        String line = "";
+        String cardLine = "";
         try {
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String currentLine = s.nextLine();
-                line += currentLine.substring(0, currentLine.indexOf("|")) + "\n";
+                cardLine += currentLine.substring(0, currentLine.indexOf("|")) + "\n";
             }
 
-            String[] array = line.split("\n");
+            String[] array1 = cardLine.split("\n");
 
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < array1.length; i++) {
                 // split by space, now we have a list of String numbers
-                hand[i] = Arrays.toString((array[i]).split(" "));
+                hand[i] = Arrays.toString((array1[i]).split(" "));
             }
             return hand;
         } catch (FileNotFoundException fe) {
@@ -35,7 +39,31 @@ public class Main {
             System.exit(1);
         }
         return hand;
+    }
 
+    // METHOD: all bets are made into an INTEGER array
+    public static int[] getBet(String fileName) {
+        int[] bet = new int[79];
+        File f = new File("src/input_file");
 
+        String betLine = "";
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String currentLine = s.nextLine();
+                betLine += currentLine.substring(currentLine.indexOf("|") + 1) + "\n";
+            }
+            String[] array2 = betLine.split("\n");
+
+            for (int i = 0; i < array2.length; i++) {
+                // split by space, now we have a list of String numbers
+                bet[i] = Integer.parseInt(array2[i]);
+            }
+            return bet;
+        } catch (FileNotFoundException fe) {
+            System.out.println("File was not found");
+            System.exit(1);
+        }
+        return bet;
     }
 }
--- src/Poker.java
+++ src/Poker.java
@@ -1,35 +1,14 @@
 public class Poker {
-    private String[] bid;
+    private int[] bid;
     private String[] cards;
-    private int group1;
-    private int group2;
 
- public Poker (String[] cards){
+ public Poker (String[] cards, int[] bid){
      this.cards = cards;
-     group1 = 0;
-     group2 = 0;
+     this.bid = bid;
  }
- //11234
 
- public String analyzeHand(){
-     String card1 = "";
-     String card2 = "";
-     String card3 = "";
-     String card4 = "";
-     String card5 = "";
-     int ace =0;
-     int king = 0;
-     int queen = 0;
-     int jack = 0;
-     int ten = 0;
-     int nine = 0;
-     int eight = 0;
-     int seven = 0;
-     int six = 0;
-     int five = 0;
-     int four = 0;
-     int three = 0;
-     int two = 0;
+ public String analyzeHand(){  // part 1
+     // sorts of deck hands
      int fiveOfAKind = 0;
      int fourOfAKind = 0;
      int fullHouse = 0;
@@ -37,89 +16,105 @@ public class Poker {
      int twoPair = 0;
      int onePair = 0;
      int highCard = 0;
+
+     // putting each element in the line into a specific card number
     for (String pile : cards){
-        card1 = pile.substring(0,(pile.indexOf(",")));
+        // types of cards
+        int ace = 0;
+        int king = 0;
+        int queen = 0;
+        int jack = 0;
+        int ten = 0;
+        int nine = 0;
+        int eight = 0;
+        int seven = 0;
+        int six = 0;
+        int five = 0;
+        int four = 0;
+        int three = 0;
+        int two = 0;
+
+        String card1 = pile.substring(0,(pile.indexOf(",")));
         pile = pile.substring((pile.indexOf(",")));
-        card2 = pile.substring(0,(pile.indexOf(",")));
+        String card2 = pile.substring(0,(pile.indexOf(",")));
         pile = pile.substring((pile.indexOf(",")));
-        card3 = pile.substring(0,(pile.indexOf(",")));
+        String card3 = pile.substring(0,(pile.indexOf(",")));
         pile = pile.substring((pile.indexOf(",")));
-        card4 = pile.substring(0,(pile.indexOf(",")));
+        String card4 = pile.substring(0,(pile.indexOf(",")));
         pile = pile.substring((pile.indexOf(",")));
-        card5 = pile;
+        String card5 = pile;
+
+
+        // new array made for the single line
+        String[] hand = {card1, card2, card3, card4, card5};
+        for (String each: hand){
+            if (each.equals("Ace")) {
+                ace++;
+            }
+            else if (each.equals("King")) {
+                king++;
+            }
+            else if (each.equals("Queen")) {
+                queen++;
+            }
+            else if (each.equals("Jack")) {
+                jack++;
+            }
+            else if (each.equals("10")) {
+                ten++;
+            }
+            else if (each.equals("9")) {
+                nine++;
+            }
+            else if (each.equals("8")) {
+                eight++;
+            }
+            else if (each.equals("7")) {
+                seven++;
+            }
+            else if (each.equals("6")) {
+                six++;
+            }
+            else if (each.equals("5")) {
+                five++;
+            }
+            else if (each.equals("4")) {
+                four++;
+            }
+            else if (each.equals("3")) {
+                three++;
+            }
+            else if (each.equals("2")) {
+                two++;
+            }
+        }
+
+        if (ace == 5 || king == 5 || queen == 5 || jack == 5 || ten == 5 || nine == 5 || eight == 5 || seven == 5 || six == 5 || five == 5 || four == 5 || three == 5 || two == 5) {
+            fiveOfAKind++;
+        } else if (ace == 4 || king == 4 || queen == 4 || jack == 4 || ten == 4 || nine == 4 || eight == 4 || seven == 4 || six == 4 || five == 4 || four == 4 || three == 4 || two == 4) {
+            fourOfAKind++;
+        } else if ((ace == 3 || king == 3 || queen == 3 || jack == 3 || ten == 3 || nine == 3 || eight == 3 || seven == 3 || six == 3 || five == 3 || four == 3 || three == 3 || two == 3) &&
+                (ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2)) {
+            fullHouse++;
+        } else if (ace == 3 || king == 3 || queen == 3 || jack == 3 || ten == 3 || nine == 3 || eight == 3 || seven == 3 || six == 3 || five == 3 || four == 3 || three == 3 || two == 3) {
+            threeOfAKind++;
+        } else if ((ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2) &&
+                (ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2)) {
+            twoPair++;
+        } else if (ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2) {
+            onePair++;
+        } else {
+            highCard++;
+        }
     }
 
-    String[] hand = {card1,card2,card3,card4,card5};
-   for (String each: hand){
-       if (each.equals("Ace")) {
-           ace++;
-       }
-       if (each.equals("King")) {
-           king++;
-       }
-       if (each.equals("Queen")) {
-           queen++;
-       }
-       if (each.equals("Jack")) {
-           jack++;
-       }
-       if (each.equals("10")) {
-           ten++;
-       }
-       if (each.equals("9")) {
-           nine++;
-       }
-       if (each.equals("8")) {
-           eight++;
-       }
-       if (each.equals("7")) {
-           seven++;
-       }
-       if (each.equals("6")) {
-           six++;
-       }
-       if (each.equals("5")) {
-           five++;
-       }
-       if (each.equals("4")) {
-           four++;
-       }
-       if (each.equals("3")) {
-           three++;
-       }
-       if (each.equals("2")) {
-           two++;
-       }
-   }
-
-     if (ace == 5 || king == 5 || queen == 5 || jack == 5 || ten == 5 || nine == 5 || eight == 5 || seven == 5 || six == 5 || five == 5 || four == 5 || three == 5 || two == 5) {
-         fiveOfAKind++;
-     } else if (ace == 4 || king == 4 || queen == 4 || jack == 4 || ten == 4 || nine == 4 || eight == 4 || seven == 4 || six == 4 || five == 4 || four == 4 || three == 4 || two == 4) {
-         fourOfAKind++;
-     } else if ((ace == 3 || king == 3 || queen == 3 || jack == 3 || ten == 3 || nine == 3 || eight == 3 || seven == 3 || six == 3 || five == 3 || four == 3 || three == 3 || two == 3) &&
-             (ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2)) {
-         fullHouse++;
-     } else if (ace == 3 || king == 3 || queen == 3 || jack == 3 || ten == 3 || nine == 3 || eight == 3 || seven == 3 || six == 3 || five == 3 || four == 3 || three == 3 || two == 3) {
-         threeOfAKind++;
-     } else if ((ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2) &&
-             (ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2) &&
-             (ace != 3 && king != 3 && queen != 3 && jack != 3 && ten != 3 && nine != 3 && eight != 3 && seven != 3 && six != 3 && five != 3 && four != 3 && three != 3 && two != 3)) {
-         twoPair++;
-     } else if (ace == 2 || king == 2 || queen == 2 || jack == 2 || ten == 2 || nine == 2 || eight == 2 || seven == 2 || six == 2 || five == 2 || four == 2 || three == 2 || two == 2) {
-         onePair++;
-     } else {
-         highCard++;
-     }
-
-     return "Number of five of a kind hands: "+fiveOfAKind+"\n"+
-     "Number of full house hands: "+fullHouse+"\n"+
-     "Number of four of a kind hands: "+fourOfAKind+"\n"+
-     "Number of three of a kind hands: "+threeOfAKind+"\n"+
-     "Number of two pair hands: " +twoPair+"\n"+
-     "Number of one pair hands: " +onePair+"\n"+
-     "Number of high card hands: "+highCard;
-
-
+     return "Number of five of a kind hands: " + fiveOfAKind + "\n"+
+     "Number of full house hands: " + fullHouse + "\n" +
+     "Number of four of a kind hands: " + fourOfAKind + "\n" +
+     "Number of three of a kind hands: " + threeOfAKind + "\n" +
+     "Number of two pair hands: " + twoPair + "\n" +
+     "Number of one pair hands: " + onePair + "\n" +
+     "Number of high card hands: " + highCard;
  }
 
 }
